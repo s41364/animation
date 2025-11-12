2060c35697a968cbc037d15d798ae88f90a867e7
หมายิ้ม <s41364@bj.ac.th>
2025-11-12 11:14:53 +0700

    Add files via upload

---
+++ a1_platform_22/offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1762920911,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bg3-sheet0.png",
+		"images/player2-sheet0.png",
+		"images/coin2-sheet0.png",
+		"images/coinbox-sheet0.png",
+		"images/tiledbackground.png",
+		"images/sprite-sheet0.png",
+		"images/eiei.png",
+		"images/sprite2-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
